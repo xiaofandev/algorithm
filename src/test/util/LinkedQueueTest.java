c62927f8ad5eb8c30c081fee0eddefcef593573d
xiaofandev <xiaofandev@126.com>
2022-05-07 22:26:29 +0800

    Update LinkedQueue

--- src/test/util/LinkedQueueTest.java
+++ src/test/util/LinkedQueueTest.java
@@ -6,13 +6,31 @@ import util.LinkedQueue;
 public class LinkedQueueTest {
 
 	public static void main(String[] args) {
-		test();
+		testEnqueue();
+		testDequeue();
 	}
 	
-	public static void test() {
-		LinkedQueue queue = new LinkedQueue();
+	public static void testEnqueue() {
+		LinkedQueue<String> queue = new LinkedQueue<>();
 		queue.enqueue("aa");
+		queue.enqueue("bb");
+		Assert.success();
+	}
+	
+	/**
+	 * description: on condition of queue is not empty and queue is empty
+	 * 
+	 */
+	public static void testDequeue() {
+		LinkedQueue<String> queue = new LinkedQueue<>();
+		queue.enqueue("aa");
+		queue.enqueue("bb");
+		// not empty
 		Assert.equal(queue.dequeue(), "aa");
+		Assert.equal(queue.dequeue(), "bb");
+		// empty
+		Assert.equal(queue.dequeue(), null);
+		
 	}
 	
 }
--- src/util/LinkedQueue.java
+++ src/util/LinkedQueue.java
@@ -1,15 +1,50 @@
 package util;
 
-public class LinkedQueue {
+public class LinkedQueue<E> {
 
-	private LinkedList<String> list = new LinkedList<>();
+	private Node<E> head;
+	private Node<E> tail;
 	
-	public void enqueue(String e) {
-		list.addLast(e);
+	public void enqueue(E e) {
+		if(tail == null) {
+			final Node<E> newNode = new Node<>(null, e, null);
+			tail = newNode;
+			head = newNode;
+		} else {
+			final Node<E> prev = tail;
+			final Node<E> node = new Node<>(prev, e, null);
+			prev.next = node;
+			tail = node;
+		}
 	}
 	
-	public String dequeue() {
-		return list.removeFirst();
+	public E dequeue() {
+		if(head == null) {
+			return null;
+		} else {
+			final Node<E> h = head;
+			final Node<E> next = h.next;
+			h.next = null;
+			head = next;
+			if(next == null) {
+				tail = null;
+			} else {
+				next.prev = null;
+			}
+			return h.element;
+		}
+	}
+	
+	private static class Node<E> {
+		Node<E> prev;
+		E element;
+		Node<E> next;
+		
+		public Node(Node<E> prev, E e, Node<E> next) {
+			this.prev = prev;
+			this.element = e;
+			this.next = next;
+		}
 	}
 	
 }
