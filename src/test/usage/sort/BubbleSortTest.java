1b140c11e6a03da321f6f2c4e5ad8a16d0d8c92c
xiaofandev <xiaofandev@126.com>
2022-05-10 07:35:32 +0800

    Create BubbleSortTest.java

---
+++ src/test/usage/sort/BubbleSortTest.java
@@ -0,0 +1,20 @@
+package test.usage.sort;
+
+import test.Assert;
+import usage.sort.BubbleSort;
+
+public class BubbleSortTest {
+
+	public static void main(String[] args) {
+		testSort();
+	}
+	
+	public static void testSort() {
+		Integer[] data1 = new Integer[] {3, 1, 5, 4, 2};
+		Assert.equal(BubbleSort.sort(data1), new Integer[] {1,2,3,4,5});
+		
+		Integer[] data2 = new Integer[] {23, 15, 2, 7, 21};
+		Assert.equal(BubbleSort.sort(data2), new Integer[] {2,7,15,21,23});
+	}
+
+}
