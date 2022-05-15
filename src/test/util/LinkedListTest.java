a6b3adc5ff468b689c340e8dd88a73d40bb29f4e
xiaofandev <xiaofandev@126.com>
2022-05-15 20:25:09 +0800

    Add testAddFirst method

--- src/test/util/LinkedListTest.java
+++ src/test/util/LinkedListTest.java
@@ -1,6 +1,22 @@
 package test.util;
 
+import test.Assert;
+import util.LinkedList;
+
 public class LinkedListTest {
 
+	public static void main(String[] args) {
+		testAddFirst();
+	}
+	
+	public static void testAddFirst() {
+		LinkedList<Integer> list = new LinkedList<>();
+		list.addLast(1);
+		Assert.equal(list.get(0), 1);
+		list.addLast(2);
+		Assert.equal(list.get(1), 2);
+		list.addLast(3);
+		Assert.equal(list.get(2), 3);
+	}
 	
 }
